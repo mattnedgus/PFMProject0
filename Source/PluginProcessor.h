6118f39d484e8e30392490af05fe35abec60548d
mattnedgus <mmsugden@gmail.com>
2021-07-14 22:47:12 +0100

    Created Random Noise Sound

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -6,6 +6,15 @@
   ==============================================================================
 */
 
+
+/*
+TODO:
+click anywhere on the window, and play a note
+if you click and drag, it will change the pitch of the note
+SHould we play a sound?
+*/
+
+
 #pragma once
 
 #include <JuceHeader.h>
@@ -54,6 +63,8 @@ public:
     void setStateInformation (const void* data, int sizeInBytes) override;
 
 private:
+    bool shouldPlaySound = true;
+
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PFMProject0AudioProcessor)
 };
